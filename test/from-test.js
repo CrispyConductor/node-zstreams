d3b2d8a7f374dd5c2f3b6eed3169aa29a043733a
Andrew Oppenlander <andrew.oppenlander@zipscene.com>
2015-02-18 16:01:28 -0500

    Started work on fromFunction/FucntionStream

---
+++ test/from-test.js
@@ -0,0 +1,19 @@
+var expect = require('chai').expect;
+
+var zstreams = require('../lib');
+
+describe('fromArray', function() {
+
+});
+
+describe('fromFile', function() {
+
+});
+
+describe('fromFunction', function() {
+
+});
+
+describe('fromFunctionSync', function() {
+
+});
