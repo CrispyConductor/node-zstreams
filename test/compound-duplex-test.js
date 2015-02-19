9ba57f461f69463a583f203b4505d3e286772002
Chris Breneman <crispy@cluenet.org>
2015-02-19 15:36:47 -0500

    Compound duplex streams and some other things

---
+++ test/compound-duplex-test.js
@@ -0,0 +1,50 @@
+var expect = require('chai').expect;
+
+var zstreams = require('../lib');
+var CompoundDuplex = zstreams.CompoundDuplex;
+var PassThrough = zstreams.PassThrough;
+var SplitStream = zstreams.SplitStream;
+var inherits = require('util').inherits;
+
+describe('CompoundDuplex', function() {
+
+	it('should encapsulate multiple streams', function(done) {
+
+		function TestCompoundDuplexStream() {
+			CompoundDuplex.call(this,
+				new PassThrough()
+				.pipe(new SplitStream(' '))
+				.throughSync(function(str) {
+					return str.toUpperCase();
+				})
+			);
+		}
+		inherits(TestCompoundDuplexStream, CompoundDuplex);
+
+		zstreams.fromString('Hello World').pipe(new TestCompoundDuplexStream()).toArray(function(error, array) {
+			expect(error).to.not.exist;
+			expect(array).to.deep.equal(['HELLO', 'WORLD']);
+			done();
+		});
+
+	});
+
+	it('should forward errors from encapsulated streams', function(done) {
+		function TestCompoundDuplexStream() {
+			CompoundDuplex.call(this,
+				new PassThrough()
+				.pipe(new SplitStream(' '))
+				.throughSync(function(str) {
+					throw new Error('Test error');
+				})
+			);
+		}
+		inherits(TestCompoundDuplexStream, CompoundDuplex);
+
+		zstreams.fromString('Hello World').pipe(new TestCompoundDuplexStream()).toArray(function(error, array) {
+			expect(error).to.exist;
+			done();
+		});
+	});
+
+});
