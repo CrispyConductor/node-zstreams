839544bb394b91f058ef48b232f55b7738ae399a
Chris Crowell <chris.crowell@zipscene.com>
2015-02-26 16:19:51 -0500

    Fix writable write, add tests for duplex read and write

--- test/convenience-constructor-test.js
+++ test/convenience-constructor-test.js
@@ -22,6 +22,7 @@ describe('iojs simplified stream constructors', function() {
 				readStream.intoString(function(error, str) {
 					expect(error).to.not.exist;
 					expect(str).to.have.length(4);
+					expect(str).to.be.a('string');
 					done();
 				});
 			});
@@ -48,125 +49,164 @@ describe('iojs simplified stream constructors', function() {
 		});
 	});
 
-	describe('Writable._write', function () {
-		describe('data mode', function() {
-			it('should receive the Readable stream', function(done) {
-				var writeStream = new Writable({
-					write: function(chunk, encoding, cb) {
-						cb();
-					}
-				});
-				count = 0;
-				zstreams.fromString('abcd', {chunkSize: 1}).pipe(writeStream).intoCallback(function(error) {
-					expect(error).to.not.exist;
-					count++;
-					expect(count).to.equal(1);
-					done();
-				});
+describe('Writable._write', function () {
+	describe('data mode', function() {
+		it('should receive the Readable stream', function(done) {
+			var writeStream = new Writable({
+				write: function(chunk, encoding, cb) {
+					cb();
+				}
+			});
+			count = 0;
+			zstreams.fromString('abcd', {chunkSize: 1}).pipe(writeStream).intoCallback(function(error) {
+				expect(error).to.not.exist;
+				count++;
+				expect(count).to.equal(1);
+				done();
 			});
 		});
-		describe('object mode', function() {
-			it('should receive the Readable stream', function(done) {
-				var writeStream = new Writable({
-					objectMode: true,
-					write: function(chunk, encoding, cb) {
-						cb();
-					}
-				});
-				count = 0;
-				zstreams.fromString('abcd', {chunkSize: 1}).pipe(writeStream).intoCallback(function(error) {
-					expect(error).to.not.exist;
-					count++;
-					expect(count).to.equal(1);
-					done();
-				});
+	});
+	describe('object mode', function() {
+		it('should receive the Readable stream', function(done) {
+			var writeStream = new Writable({
+				objectMode: true,
+				write: function(chunk, encoding, cb) {
+					cb();
+				}
+			});
+			count = 0;
+			zstreams.fromString('abcd', {chunkSize: 1}).pipe(writeStream).intoCallback(function(error) {
+				expect(error).to.not.exist;
+				count++;
+				expect(count).to.equal(1);
+				done();
+			});
+		});
+	});
+});
+
+
+//TODO: add test for write flush when supported
+
+describe('Duplex._read', function() {
+	describe('data mode', function() {
+		it('should match what the writable contains', function(done) {
+			var duplexStream = new Duplex({
+				read: function() {
+					this.push('a');
+					this.push('b');
+					this.push('c');
+					this.push('d');
+					this.push(null);
+				}
+			});
+			duplexStream.intoString(function(error, str) {
+				expect(error).to.not.exist;
+				expect(str).to.have.length(4);
+				expect(str).to.be.a('string');
+				done();
 			});
 		});
 	});
 
-//How do I use flush?
-/*
-describe('Writable._flush', function () {
-	it('should receive the Readable stream', function() {
-		var writeStream = new Writable({
-			flush: function() {
-			}
+	describe('object mode', function() {
+		it('should match what the writable contains', function(done) {
+			var duplexStream = new Duplex({
+				objectMode: true,
+				read: function() {
+					this.push('a');
+					this.push('b');
+					this.push('c');
+					this.push('d');
+					this.push(null);
+				}
+			});
+			duplexStream.intoString(function(error, str) {
+				expect(error).to.not.exist;
+				expect(str).to.have.length(4);
+				expect(str).to.be.a('string');
+				done();
+			});
 		});
-		zstreams.fromString('beep boop\n').pipe(writeStream).intoCallback(function(error) {
-			expect(error).to.not.exist;
-
-			var str = writeStream.getString();
-			expect(str).to.equal('abcd');
-
-			done();
-		});
-	});	
-});
-*/
-
-/*
-describe('Duplex._read', function () {
-	it('should match what the writable contains', function(done) {
-		var duplexStream = new Duplex({
-			read: function() {
-				this.push('a');
-				this.push('b');
-				this.push('c');
-				this.push('d');
-				this.push(null);
-			}
-		});
-		var writeStream = new Writable;
-		duplexStream.pipe(writeStream).intoCallback(function(error) {
-			expect(error).to.not.exist;
-
-			var str = writeStream.getString();
-			expect(str).to.equal('abcd');
-
-			done();
-		});
-	});	
+	});
 });
 
 
 describe('Duplex._write', function () {
-	it('should match wri', function(done) {
-		var writeStream = new Writable({
-			write: function() {
-
-			}
+	describe('data mode', function() {
+		it('should receive the Readable stream', function(done) {
+			var duplexStream = new Duplex({
+				write: function(chunk, encoding, cb) {
+					cb();
+				}
+			});
+			count = 0;
+			zstreams.fromString('abcd', {chunkSize: 1}).pipe(duplexStream).intoCallback(function(error) {
+				expect(error).to.not.exist;
+				count++;
+				expect(count).to.equal(1);
+				done();
+			});
 		});
-		zstreams.fromString('abcd').pipe(writeStream).intoCallback(function(error) {
-			expect(error).to.not.exist;
-
-			var str = writeStream.getString();
-			expect(str).to.equal('abcd');
-
-			done();
+	});
+	describe('object mode', function() {
+		it('should receive the Readable stream', function(done) {
+			var duplexStream = new Duplex({
+				objectMode: true,
+				write: function(chunk, encoding, cb) {
+					cb();
+				}
+			});
+			count = 0;
+			zstreams.fromString('abcd', {chunkSize: 1}).pipe(duplexStream).intoCallback(function(error) {
+				expect(error).to.not.exist;
+				count++;
+				expect(count).to.equal(1);
+				done();
+			});
 		});
-	});	
+	});
 });
 
-//TODO add test for duplex flush when supported
-
+//TODO: add test for duplex flush when supported
+/*
 describe('Transform._transform', function () {
-	it('should receive the Readable stream', function(done) {
-		var writeStream = new Writable({
-			flush: function() {
-
-			}
+	describe('data mode', function() {
+		it('should...', function(done) {
+			var transformStream = new Transform ({
+				transform: function(data, encoding, cb) {
+					cb();
+				}
+			});
+			transformStream.intoString(function(error, str) {
+				expect(error).to.not.exist;
+				expect(str).to.have.length(4);
+				expect(str).to.be.a('string');
+				done();
+			});
 		});
-		zstreams.fromString('abcd').pipe(writeStream).intoCallback(function(error) {
-			expect(error).to.not.exist;
-
-			var str = writeStream.getString();
-			expect(str).to.equal('abcd');
-
-			done();
+	});
+	/*
+	describe('object mode', function() {
+		it('should receive the Readable stream', function(done) {
+			var transformStream = new Transform ({
+				objectMode: true,
+				transform: function(chunk, encoding, cb) {
+					cb();
+				}
+			});
+			transformStream.intoString(function(error, str) {
+				expect(error).to.not.exist;
+				expect(str).to.have.length(0);
+				expect(str).to.be.a('string');
+				done();
+			});
 		});
-	});	
-});
+	});
+*/
+//});
 
+/*
 
 describe('Transform._flush', function () {
 	it('should receive the Readable stream', function(done) {
