4aee0e97779ae396023b7343e63079a7f509bb07
Youyou Han <youyou.han@zipscene.com>
2015-02-27 15:10:51 -0500

    wrapped JSONParseStream around jsonparse

--- test/json-stream-test.js
+++ test/json-stream-test.js
@@ -5,18 +5,17 @@ var JSONParseStream = require('../lib/streams/json-parse-stream');
 
 describe('JSONParseStream', function() {
 	it('should parse a json file into object', function(done) {
-		var readStream = fs.createReadStream('./assets/sample.json');
+		var readStream = fs.createReadStream('./test/resources/all_npm.json');
 		var jsonParseStream = new JSONParseStream();
 		readStream
 		.pipe(jsonParseStream)
 		.on('data', function (data){
-			console.log(data);
+			// console.log(data);
 			expect(data).to.be.an('object');
-			expect(data).to.have.all.keys('image', 'jumpable', 'solid', 'corners');
-			expect(data.solid).to.be.an('object');
-			expect(data.solid).to.have.all.keys('1', '2', '3', '4', '5', '6', '7', '8', '9');
-			expect(data.solid['1']).to.be.instanceof(Array);
-			expect(data.solid['1']).to.have.length(2);
+			expect(data).to.have.all.keys('total_rows', 'offset', 'rows');
+			expect(data.total_rows).to.equal(4028);
+			expect(data.rows).to.be.instanceof(Array);
+			expect(data.rows).to.have.length(4028);
 			done();
 		})
 	});
