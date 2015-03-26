6c903e3dd5dd2d567f315625385e7e2020d6c94f
Erin Thompson <erin.thompson@zipscene.com>
2015-03-26 17:16:14 -0400

    found the bug in the isClassicalStream

--- test/mixin-readable-test.js
+++ test/mixin-readable-test.js
@@ -1,12 +1,13 @@
 var expect = require('chai').expect;
-
+var fs = require('fs');
 var zstreams = require('../lib');
-var file = __dirname + '/resources/abcd.json';
+var abcdFilePath = __dirname + '/resources/abcd.json';
+var inputFilePath = __dirname + '/resources/input.json';
 
 describe('mixin/_readable', function() {
 	describe('through', function() {
 		it('should get all the data and not throw an error', function(done) {
-			zstreams.fromFile(file).through(function(obj, cb) {
+			zstreams.fromFile(abcdFilePath).through(function(obj, cb) {
 				expect(obj.toString()).to.equal('"a"\n"b"\n"c"\n"d"\n');
 				cb(null, obj);
 			}).intoString(function(error) {
@@ -16,7 +17,7 @@ describe('mixin/_readable', function() {
 		});
 
 		it('should catch thrown errors and covert to stream errors', function(done) {
-			zstreams.fromFile(file).through(function(obj, cb) {
+			zstreams.fromFile(abcdFilePath).through(function(obj, cb) {
 				throw new Error();
 			}).intoString(function(error) {
 				expect(error).to.exist;
@@ -27,7 +28,7 @@ describe('mixin/_readable', function() {
 
 	describe('throughSync', function() {
 		it ('should it should catch a thrown error and covert to stream errors', function(done) {
-			zstreams.fromFile(file).split().throughSync(function(obj) {
+			zstreams.fromFile(abcdFilePath).split().throughSync(function(obj) {
 				throw new Error();
 			}).intoString(function(error) {
 				expect(error).to.exist;
@@ -36,12 +37,32 @@ describe('mixin/_readable', function() {
 		});
 		it ('should return the first line of the file', function(done) {
 			var lines = [];
-			zstreams.fromFile(file).split().throughSync(function(chunk) {
+			zstreams.fromFile(abcdFilePath).split().throughSync(function(chunk) {
 				lines.push(chunk);
 				return chunk;
 			}).intoArray(function(error) {
 				expect(lines[0]).to.equal('"a"');
 				done();
+			});
+		});
+	});
+
+	describe('intoFile', function() {
+		it ('should write to a file and call a callback', function(done) {
+			this.timeout(30000);
+			var inputFileSize, abcdFileSize;
+			zstreams.fromFile(abcdFilePath).throughSync(function(chunk) {
+				return chunk.toString('utf8').toUpperCase();
+			}).intoFile(inputFilePath, function(error) {
+				expect(error).to.not.exist;
+				fs.stat(inputFilePath, function(error, stats) {
+					inputFileSize = stats.size / 1000000000;
+					fs.stat(abcdFilePath, function(error, stats) {
+						abcdFileSize = stats.size / 1000000000;
+						expect(abcdFileSize).to.equal(inputFileSize);
+						done();
+					});
+				});
 			});
 		});
 	});
