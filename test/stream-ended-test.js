801871d7d8cd8de2b53e26b0c54c12a6a5b289d3
Chris Crowell <chris.crowell@zipscene.com>
2015-04-28 11:38:56 -0400

    Add support for stream returning after it has finished with error

--- test/stream-ended-test.js
+++ test/stream-ended-test.js
@@ -4,96 +4,83 @@ var zstreams = require('../lib');
 var Writable = zstreams.Writable;
 
 describe('Stream ended into functions', function() {
-	it.skip('intoCallback should resolve', function(done) {
-		var writeStream = new Writable({
-			write: function(chunk, encoding, cb) {
-				cb();
-			}
-		});
-		zstreams.fromString('abcd').pipe(writeStream);
-		setTimeout(function() {
-			writeStream.intoCallback(function(error) {
-				expect(error).to.not.exist;
-				done();
+	describe('Should return normally with no errors', function() {
+		it('intoCallback should return', function(done) {
+			var writeStream = new Writable({
+				write: function(chunk, encoding, cb) {
+					cb();
+				}
 			});
-		}, 500);
-	});
-
-	it('intoCallback error', function(done) {
-		var writeStream = new Writable({
-			write: function(chunk, encoding, cb) {
-				console.log('heyooo');
-				cb();
-			}
-		});
-
-		console.log('should be inbetween?');
-
-		zstreams.fromString('abcd')
-			.through(function(chunk, encoding, cb) {
-				throw new Error(123);
-				cb();
-			})
-			.pipe(writeStream);
-
-		console.log('And now we wait...');
-		setTimeout(function() {
-			writeStream.intoCallback(function(error) {
-				console.log('error: ' + error);
-				expect(error).to.exist;
-				done();
-			});
-		}, 500);
-	});
-
-	it.skip('intoCallback error control', function(done) {
-		var writeStream = new Writable({
-			write: function(chunk, encoding, cb) {
-				cb();
-			}
-		});
-
-		zstreams.fromString('abcd')
-			.through(function(chunk, encoding, cb) {
-				throw new Error(123);
-			})
-			.pipe(writeStream)
-			.intoCallback(function(error) {
-				console.log('error: ' + error);
-				expect(error).to.exist;
-				done();
-			});
-	});
-
-	it.skip('intoPromise should resolve', function(done) {
-		var writeStream = new Writable({
-			write: function(chunk, encoding, cb) {
-				cb();
-			}
-		});
-		zstreams.fromString('abcd').pipe(writeStream);
-		setTimeout(function() {
-			writeStream.intoPromise()
-				.then(function(err, result) {
-					console.log('result: ' + result);
+			zstreams.fromString('abcd').pipe(writeStream);
+			setTimeout(function() {
+				writeStream.intoCallback(function(error) {
+					expect(error).to.not.exist;
 					done();
 				});
-		}, 500);
+			}, 200);
+		});
+
+		it('intoPromise should resolve', function(done) {
+			var writeStream = new Writable({
+				write: function(chunk, encoding, cb) {
+					cb();
+				}
+			});
+			zstreams.fromString('abcd').pipe(writeStream);
+			setTimeout(function() {
+				writeStream.intoPromise()
+					.then(function(error) {
+						expect(error).to.not.exist;
+						done();
+					});
+			}, 200);
+		});
 	});
 
-	it.skip('intoPromise with error', function(done) {
-		var writeStream = new Writable({
-			write: function(chunk, encoding, cb) {
-				throw new Error(123);
-			}
-		});
-		zstreams.fromString('abcd').pipe(writeStream);
-		setTimeout(function() {
-			writeStream.intoPromise()
-				.then(function(err, result) {
-					console.log('result: ' + result);
+	describe('Should return with errors', function() {
+		it('intoCallback error', function(done) {
+			var writeStream = new Writable({
+				write: function(chunk, encoding, cb) {
+					cb();
+				}
+			});
+
+			zstreams.fromString('abcd')
+				.through(function() {
+					throw new Error(123);
+				})
+				.pipe(writeStream);
+
+			setTimeout(function() {
+				writeStream.intoCallback(function(error) {
+					expect(error).to.exist;
 					done();
 				});
-		}, 500);
+			}, 200);
+		});
+
+		it('intoPromise with error', function(done) {
+			var writeStream = new Writable({
+				write: function(chunk, encoding, cb) {
+					cb();
+				}
+			});
+
+			zstreams.fromString('abcd')
+				.through(function() {
+					throw new Error(123);
+				})
+				.pipe(writeStream);
+
+			setTimeout(function() {
+				writeStream.intoPromise()
+					.then(function() {
+						//placeholder
+					}, function(error) {
+						expect(error).to.exist;
+						done();
+					}).catch(done);
+			}, 200);
+		});
 	});
 });
